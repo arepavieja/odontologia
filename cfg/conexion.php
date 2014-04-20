98140fd72ded486547939c982bf7a9f47d52b947
publio <publio.quintero@gmail.com>
2014-04-20 15:04:49 -0430

    200420141530
    1.- Borrar método de PHPMailer en la conexión

--- cfg/conexion.php
+++ cfg/conexion.php
@@ -20,35 +20,6 @@ class Conexion {
 		}
 	}
 
-	/*function phpMailer($destino,$cuerpo) {
-		$this->mail = new PHPMailer();
-		$this->mail->IsSMTP();
-		$this->mail->SMTPAuth = true;
-		#$mail->SMTPSecure = 'tls';
-		$this->mail->Host = "ssl://smtp.gmail.com"; // SMTP a utilizar. Por ej. smtp.elserver.com
-		$this->mail->Username = "publio.quintero@gmail.com"; // Correo completo a utilizar
-		$this->mail->Password = "servinet511!!!"; // Contraseña
-		$this->mail->Port = 465; // Puerto a utilizar
-		//Con estas pocas líneas iniciamos una conexión con el SMTP. Lo que ahora deberíamos hacer, es configurar el mensaje a enviar, el //From, etc.
-		$this->mail->From = "publio.quintero@gmail.com"; // Desde donde enviamos (Para mostrar)
-		$this->mail->FromName = "Nombre";
-
-		//Estas dos líneas, cumplirían la función de encabezado (En mail() usado de esta forma: “From: Nombre <correo@dominio.com>”) de //correo.
-		$this->mail->AddAddress($destino); // Esta es la dirección a donde enviamos
-		$this->mail->IsHTML(true); // El correo se envía como HTML
-		$this->mail->Subject = 'Titulo'; // Este es el titulo del email.
-		$body = $cuerpo;
-		$this->mail->Body = $body; // Mensaje a enviar
-		$exito = $this->mail->Send(); // Envía el correo.
-
-		//También podríamos agregar simples verificaciones para saber si se envió:
-		if($exito){
-		return 'El correo fue enviado correctamente.';
-		}else{
-		return 'Hubo un inconveniente. Contacta a un administrador.';
-		}
-	}*/
-
 	function compara($tabla,$campo,$valor) {
 		$con = $this->pgsql();
 		$sql = "SELECT ".$campo." FROM ".$tabla." WHERE ".$campo."=?";
